5649abeb8b034c0360997df2da2c2a407efb1fc7
Nicolás González <nicolasnicogonzalez@gmail.com>
2017-10-05 16:02:43 -0400

    New teleop

--- ftc_app-2.2.1/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/opcodes/FxDTeleop.java
+++ ftc_app-2.2.1/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/opcodes/FxDTeleop.java
@@ -10,7 +10,7 @@ public class FxDTeleop extends OpMode {
     // Speeds
     final double[] speeds = new double[]{0.3, 0.6, 1.0};
 
-    int speedIndex = 2, direction = 1;
+    int speedIndex = 2;
 
     // States
     boolean[] lastPressed = new boolean[]{false, false};
@@ -18,10 +18,10 @@ public class FxDTeleop extends OpMode {
     @Override
     public void init() {
         // Get the hardware from the robot configuration
-        motors[0] = hardwareMap.dcMotor.get("wheel0");
-        motors[1] = hardwareMap.dcMotor.get("wheel1");
-        motors[2] = hardwareMap.dcMotor.get("wheel2");
-        motors[3] = hardwareMap.dcMotor.get("wheel3");
+        motors[0] = hardwareMap.dcMotor.get("Lwheel1");
+        motors[1] = hardwareMap.dcMotor.get("Lwheel2");
+        motors[2] = hardwareMap.dcMotor.get("Rwheel1");
+        motors[3] = hardwareMap.dcMotor.get("Rwheel2");
     }
 
     @Override
@@ -29,39 +29,27 @@ public class FxDTeleop extends OpMode {
         //region Wheel Movement
         // Rotate right
         if (gamepad1.right_trigger != 0) {
-            for (DcMotor motor : motors)
-                motor.setPower(gamepad1.right_trigger * speeds[speedIndex]);
+            motors[0].setPower(gamepad1.right_trigger * speeds[speedIndex]);
+            motors[1].setPower(gamepad1.right_trigger * speeds[speedIndex]);
         }
         // Rotate left
         else if (gamepad1.left_trigger != 0) {
-            for (DcMotor motor : motors)
-                motor.setPower(-gamepad1.left_trigger * speeds[speedIndex]);
+            motors[1].setPower(gamepad1.left_trigger * speeds[speedIndex]);
+            motors[2].setPower(gamepad1.left_trigger * speeds[speedIndex]);
         }
         // Move at specific angle
         else {
-            // TODO Add direction
-            //move forwards
-            motors[0].setPower(-direction * gamepad1.left_stick_x * speeds[speedIndex]);
-            motors[1].setPower(direction * gamepad1.left_stick_x * speeds[speedIndex]);
+            motors[0].setPower(gamepad1.left_stick_x * speeds[speedIndex]);
+            motors[1].setPower(gamepad1.left_stick_x * speeds[speedIndex]);
 
-            //move backwards
-            motors[2].setPower(-direction * gamepad1.left_stick_y * speeds[speedIndex]);
-            motors[3].setPower(direction * gamepad1.left_stick_y * speeds[speedIndex]);
+            motors[2].setPower(gamepad1.left_stick_x * speeds[speedIndex]);
+            motors[3].setPower(gamepad1.left_stick_x * speeds[speedIndex]);
         }
 
         if (gamepad1.left_bumper && !lastPressed[0] && speedIndex > 0) {
             speedIndex--;
         } else if (gamepad1.right_bumper && !lastPressed[0] && speedIndex < speeds.length - 1) {
             speedIndex++;
-        }
-        //endregion
-
-        //region DPad
-        // DPad for direction
-        if (gamepad1.dpad_up) {
-            direction = -1;
-        } else if (gamepad1.dpad_down) {
-            direction = 1;
         }
         //endregion
 
